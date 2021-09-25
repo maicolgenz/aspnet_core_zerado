2237ca84238b29e5928f7eb2dda25b3b6a6b0259
Maicol <Maicol@DESKTOP-21K4BMD>
2021-09-25 09:35:13 -0300

    ajustes

--- App.Web/wwwroot/js/pages/pessoa.js
+++ App.Web/wwwroot/js/pages/pessoa.js
@@ -5,9 +5,6 @@
         }
     });
     load();
-    $('[name="telefone"]').mask('9999-9999');
-
-    Mascaras();
 });
 
 function load() {
@@ -26,30 +23,5 @@ function load() {
                 '<td>' + (obj.ativo === true ? 'Ativo' : 'Inativo') + '</td>' +
                 '</tr>');
         });
-    });
-}
-
-//$("#telefone").inputmask({
-//    mask: "(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})",
-//});]
-
-
-function Mascaras() {
-    $("[data-mascara='date']").each(function () { $(this).mask('00/00/0000'); });
-    $("[data-mascara='datetime']").each(function () { $(this).mask('00/00/0000 00:00'); });
-    $("[data-mascara='cep']").each(function () { $(this).mask('00000-000'); });
-    $("[data-mascara='phone']").each(function () { $(this).mask('(00) 00000-0000'); });
-    $("[data-mascara='number']").each(function () { $(this).mask('0#'); });
-    $("[data-mascara='decimal']").each(function () { $(this).mask('000.000.000.000.000,00', { reverse: true }); });
-    $("[data-mascara='cpf']").each(function () { $(this).mask('000.000.000-00', { reverse: true }); });
-    $("[data-mascara='cnpj']").each(function () { $(this).mask('00.000.000/0000-00', { reverse: true }); });
-    $("[data-mascara='cpfCnpj']").each(function () {
-        var options = {
-            onKeyPress: function (cpf, ev, el, op) {
-                var masks = ['000.000.000-000', '00.000.000/0000-00'];
-                $(this).mask(cpf.length > 14 ? masks[1] : masks[0], op);
-            }
-        };
-        $(this).length > 11 ? $(this).mask('00.000.000/0000-00', options) : $(this).mask('000.000.000-00#', options);
     });
 }
